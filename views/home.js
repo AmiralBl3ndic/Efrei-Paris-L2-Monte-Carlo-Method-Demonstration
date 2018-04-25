f63ff3c9664ac35afd0a4992014e2e859928fcdd
AmiralBl3ndic <camille.briand@efrei.net>
2018-04-25 12:28:36 +0200

    Finish starting page layout and implement p5 canvas on one tab (default tab)

--- views/home.js
+++ views/home.js
@@ -1,3 +1,27 @@
+const cst = require("./../constants");
 const tabs = require("./../scripts/tabsmanager");
+const $ = require("jquery");
+
+const remote = require("electron").remote;
 
 tabs.enableTabs();
+
+
+
+// P5 functions
+function setup () {
+	
+	let canvas = createCanvas($(window).width(), $(window).height() - $("header").height());
+	
+	canvas.parent("window-section-Canvas");
+
+	background(120, 120, 120);
+}
+
+function draw () {
+	if (mouseIsPressed) {
+		fill(0);
+		
+		ellipse(mouseX, mouseY, 25, 25);
+	}
+}
