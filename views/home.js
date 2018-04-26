fe4ae88c0b4e8e46b586607918fddcb2b095ad12
AmiralBl3ndic <camille.briand@efrei.net>
2018-04-26 16:55:03 +0200

    (Big commit) - Change the views/home.html file so that it matches the wanted layout - Create the corresponding views/home.js renderer script     * Uses p5.js and VueJS

--- views/home.js
+++ views/home.js
@@ -1,11 +1,16 @@
 const cst = require("./../constants");
 const tabs = require("./../scripts/tabsmanager");
 const $ = require("jquery");
+const rd = require("../scripts/random");
 
 const remote = require("electron").remote;
 
 tabs.enableTabs();
 
+const isDrawn =  (x, y) => {
+	const index = (x + y * width) * 4;
+	return pixels[index] === cst.colors.drawColor.r && pixels[index + 1] === cst.colors.drawColor.g && pixels[index + 2] === cst.colors.drawColor.b;
+};
 
 
 // P5 functions
@@ -14,14 +19,87 @@ function setup () {
 	let canvas = createCanvas($(window).width(), $(window).height() - $("header").height());
 	
 	canvas.parent("window-section-Canvas");
-
-	background(120, 120, 120);
+	
+	
+	const cl = cst.colors.backgroundColor;
+	background(cl.r, cl.g, cl.b);
 }
 
 function draw () {
 	if (mouseIsPressed) {
-		fill(0);
+		const cl = cst.colors.drawColor;
+		fill(cl.r, cl.g, cl.b);
 		
-		ellipse(mouseX, mouseY, 25, 25);
+		ellipse(mouseX, mouseY, 75, 75);
 	}
+	
+	loadPixels();
 }
+
+
+/**
+ * VueJS application (to simplify rendering data
+ */
+let settingsApp = new Vue({
+	el: "#window-section-Settings",
+	
+	data: {
+		totalPixels: $(window).width() * ($(window).height() - $("header").height()),
+		
+		classicMethod: {
+			drawnPixels: 0,
+			
+			executionTime: 0
+		},
+		
+		MonteCarloMethod: {
+			tries: 10000,
+			
+			detectedPixels: 0,
+			
+			calculatedArea: 0,
+			
+			executionTime: 0
+		}
+	},
+	
+	
+	methods: {
+		runClassic () {
+			settingsApp.classicMethod.drawnPixels = 0;
+			
+			let time = new Date().getTime();
+			
+			for (let y = 0; y < height; y++) {
+				for (let x = 0; x < width; x++) {
+					if (isDrawn(x, y)) {
+						settingsApp.classicMethod.drawnPixels++;
+					}
+				}
+			}
+			
+			settingsApp.classicMethod.executionTime = new Date().getTime() - time;
+		},
+		
+		
+		runMonteCarlo () {
+			settingsApp.MonteCarloMethod.detectedPixels = 0;
+			
+			let time = new Date().getTime();
+			
+			for (let i = 0; i < settingsApp.MonteCarloMethod.tries; i++) {
+				let x = rd.randInt(0, width - 1);
+				let y = rd.randInt(0, height - 1);
+				
+				if (isDrawn(x, y)) {
+					settingsApp.MonteCarloMethod.detectedPixels++;
+				}
+			}
+			
+			const mc = settingsApp.MonteCarloMethod;
+			mc.calculatedArea = Math.ceil((mc.detectedPixels / mc.tries) * settingsApp.totalPixels);
+			
+			settingsApp.MonteCarloMethod.executionTime = new Date().getTime() - time;
+		}
+	}
+});
