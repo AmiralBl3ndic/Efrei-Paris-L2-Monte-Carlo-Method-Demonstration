112f48710803af301cbcff6400a52bc2e330bf42
AmiralBl3ndic <camille.briand@efrei.net>
2018-06-04 15:15:42 +0200

    Change colors for background and draw

--- constants.js
+++ constants.js
@@ -7,22 +7,22 @@ exporter.verbose = false;
 
 exporter.mainWindow = {
 	window: undefined,
-	
+
 	properties: {
 		width: 1200,
 		height: 900,
-		
+
 		frame: false,
-		
+
 		movable: true,
 		resizable: false,
 		fullscreenable: false,
-		
+
 		icon: __dirname + "/icon.png"
 	},
-	
+
 	name: "Main window",
-	
+
 	url: "file://" + exporter.rootDir + "/views/home.html"
 };
 
@@ -30,15 +30,14 @@ exporter.mainWindow = {
 
 exporter.colors  = {
 	backgroundColor: {
-		r: 120,
-		g: 120,
-		b: 120
+		r: 0,
+		g: 150,
+		b: 0
 	},
-	
+
 	drawColor: {
 		r: 0,
 		g: 0,
-		b: 0
+		b: 150
 	}
 };
-
